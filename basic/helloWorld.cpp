0b0321cf3ae4327e122c848978cfb096c2cd3922
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-06-18 16:53:25 +0000

    done

---
+++ basic/helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World in C++" << endl; // aise bhi likh sakte hai
+    // or aise niche alag se bhi laga sakte hai
+    cout << endl;
+    cout << "aise bhi likh sakte hai par isme enter nahi dega";
+    // aise bhi likha jata hai
+    cout << "hi i'm raj" << endl << "i am an engineer"<<endl;
+}
+
