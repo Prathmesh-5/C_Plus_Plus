2a2f9e413b4f64e5c8503f59270298eacfad15c0
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-06-18 17:25:12 +0000

    done

---
+++ basic/number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<4*9<<endl;
+
+    int a=2,b=5,c=7,result=a*b+c;
+    cout<<"answer is : "<<result<<endl;
+    int num;
+    cout<<"Enter num value : ";
+    cin>>num;
+    
+    
+}
