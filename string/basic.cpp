ab96b1de4e28cb302e162926ef7fbb2d1e5ac2fc
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-06-22 07:12:05 +0000

    donr

---
+++ string/basic.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // printing a string 
+    string str=" *** hi my name is raj *** ";
+    cout<<str<<endl;
+
+    string x;
+    cout<<"Enter any string : ";
+    getline(cin,x);
+    cout<<"The entered string is : "<<x<<endl; // output is : prathmesh chauhan is great
+
+
+
+    // taking string as a input from  in right way
+    string y;
+    cout<<"Enter any string : "; // if i entered : prathmesh chauhan is great
+    getline(cin, y);
+    cout<<"The entered string is : "<<y<<endl; // output is : prathmesh chauhan is great
+    
+
+    return 0;
+}
