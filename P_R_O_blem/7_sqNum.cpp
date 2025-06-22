95628f2b29a2db1da466a20b4aba4af9515aceeb
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-06-22 16:09:41 +0000

    done

---
+++ P_R_O_blem/7_sqNum.cpp
@@ -0,0 +1,32 @@
+/*
+
+1 2 3 
+4 5 6 
+7 8 9
+
+*/
+
+#include<iostream>
+using namespace std;
+
+void sqNum(int n){
+    int count=0;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            count++;
+            cout<<count<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+int main(){
+    int n;
+    cout<<"Enter no. of rows : ";
+    cin>>n;
+
+    sqNum(n);
+
+    return 0;
+}
