5c1516e0898f89c99838b407c04c3bcd587bfcdd
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-06-22 16:29:39 +0000

    done

---
+++ P_R_O_blem/9_numTriPattern.cpp
@@ -0,0 +1,30 @@
+/*
+
+1
+2 2
+3 3 3
+4 4 4 4
+
+*/
+
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++)
+        cout<<i<<" ";
+    cout<<endl;
+    }
+return ;
+
+}
+int main(){
+    int n;
+    cout<<"Enter no. of rows : ";
+    cin>>n;
+
+    pattern(n);
+
+    return 0;
+}
