6ee6ba80f075d8c4097da718cbfa762c57350594
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-06-22 07:21:37 +0000

    done

---
+++ string/basic2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str="Prathmesh Chauhan";
+    int len=str.length(); // 17
+    cout<<"The length of string str is : "<<len<<endl; 
+
+    cout<<str[4]<<endl; // h
+
+
+
+    return 0;
+}
