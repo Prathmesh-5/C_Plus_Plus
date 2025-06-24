fdb62e8a7d708ac3ba2036c139a90980da53d46d
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-06-24 19:07:41 +0000

    done

---
+++ P_R_O_blem/35_rightArrowStarPattern.cpp
@@ -0,0 +1,53 @@
+/*
+
+* * * * *
+    * * * *
+        * * *
+            * *
+                *
+            * *
+        * * *
+    * * * *
+* * * * *
+  
+*/
+
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+
+    for(int i=1;i<=n;i++){
+        //space
+        for(int space=1;space<=2*(i-1);space++) cout<<"  ";
+        for(int j=1;j<=n-(i-1);j++) cout<<"* ";
+    cout<<endl;
+    }
+
+    return ;
+}
+
+void pattern_opp(int n){
+
+    for(int i=n-1;i>=1;i--){
+        //space
+        for(int space=1;space<=2*(i-1);space++) cout<<"  ";
+        for(int j=1;j<=n-(i-1);j++) cout<<"* ";
+    cout<<endl;
+    }
+
+    return ;
+}
+
+int main(){
+    int n;
+
+    cout<<"Enter no. of rows : ";
+    cin>>n;
+
+    pattern(n);
+    pattern_opp(n);
+
+
+    return 0;
+}
