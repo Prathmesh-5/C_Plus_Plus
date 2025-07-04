82eec33d6d8d8350692abcfc3107a6dce5b53568
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-07-04 16:40:50 +0000

    done

---
+++ P_R_O_blem/46_basicArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[5]={3};
+    for(int i=0;i<5;i++) cout<<arr[i]<<" "; // 3 0 0 0 0
+
+    cout<<endl;
+
+    int brr[5]={0};
+    for(int i=0;i<5;i++) cout<<brr[i]<<" "; // 0 0 0 0 0
+
+
+
+    return 0;
+}
