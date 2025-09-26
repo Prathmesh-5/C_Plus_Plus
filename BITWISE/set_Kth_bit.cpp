87642840abbeddc3986f20201102d7c7586e4eb6
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-09-26 19:41:15 +0000

    done

---
+++ BITWISE/set_Kth_bit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,k;
+    cout<<"Enter number : ";
+    cin>>x;
+    cout<<"Enter Kth index from right : ";
+    cin>>k;
+
+    cout<<( x | (1<<k))<<endl;
+
+
+    return 0;
+}
