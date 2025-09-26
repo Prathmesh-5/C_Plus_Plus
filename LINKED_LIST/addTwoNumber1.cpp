d677d16197fd5a064d511ade07168da82f0f374b
Prathmesh Chauhan <0231cse088@niet.co.in>
2025-09-26 10:12:49 +0000

    done

---
+++ LINKED_LIST/addTwoNumber1.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int value){
+        data=value;
+        next=NULL;
+    }
+};
+
+Node* addTwoNumbers(Node* Head1,Node* Head2){
+    Node* temp1=Head1;
+        Node* temp2=Head2;
+        Node* Head3=NULL;
+        Node* temp3=Head3;
+
+        int sum=0,carry=0;
+
+        while(temp1 && temp2){
+            sum=carry+temp1->data+temp2->data;
+
+            if(Head3==NULL){
+                Head3=new Node(sum%10);
+                temp3=Head3;
+            }
+            else{
+                temp3->next=new Node(sum%10);
+                temp3=temp3->next;
+            }
+
+            carry=sum/10;
+            temp1=temp1->next;
+            temp2=temp2->next;
+
+        }
+
+        while(temp1){
+            sum=carry+temp1->data;
+            temp3->next=new Node(sum%10);
+            temp3=temp3->next;
+            carry=sum/10;
+            temp1=temp1->next;
+
+        }
+
+        while(temp2){
+            sum=carry+temp2->data;
+            temp3->next=new Node(sum%10);
+            temp3=temp3->next;
+            carry=sum/10;
+            temp2=temp2->next;
+
+        }
+
+        if(carry!=0){
+            temp3->next=new Node(carry);
+            temp3=temp3->next;
+
+        }
+        temp3=Head3;
+        return Head3;
+}
+
+
+Node* createList(){
+    Node* Head=NULL;
+    Node* temp=Head;
+
+    char ch='y';
+    while(ch=='y'){
+        int value;
+            cout<<"Enter Node's value : ";
+            cin>>value;
+        if(Head==NULL){
+            
+            Head=new Node(value);
+            temp=Head;
+        }
+        else{
+            temp->next=new Node(value);
+            temp=temp->next;
+        }
+
+        cout<<"Do you want to add  more value ? (y/n) : ";
+        cin>>ch;
+
+    }
+    return Head;
+
+}
+
+
+
+int main(){
+    cout<<"**** making list1 ****"<<endl;
+    Node*Head1=NULL;
+    Node* temp1=Head1;
+
+    Node* ans1=createList();
+    Head1=ans1;
+    temp1=Head1;
+    
+
+    while(temp1){
+        cout<<temp1->data<<" -> ";
+        temp1=temp1->next;
+    }
+
+    cout<<"NULL\n";
+    temp1=Head1;
+
+    cout<<"\n**** making list1 ****"<<endl;
+    Node*Head2=NULL;
+    Node* temp2=Head2;
+
+    Node* ans2=createList();
+    Head2=ans2;
+    temp2=Head2;
+    
+
+    while(temp2){
+        cout<<temp2->data<<" -> ";
+        temp2=temp2->next;
+    }
+
+    cout<<"NULL\n";
+    temp2=Head2;
+
+    Node* ans=addTwoNumbers(Head1,Head2);
+
+    Node* TEMP=ans;
+     while(TEMP){
+        cout<<TEMP->data<<" -> ";
+        TEMP=TEMP->next;
+    }
+
+    cout<<"NULL\n";
+    TEMP=ans;
+
+
+
+    return 0;
+}
